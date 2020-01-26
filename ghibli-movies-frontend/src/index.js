329f4afec26ca833a73a52fcded4e201b5155230
chanwkkk <chanwk18@hotmail.com>
2020-01-25 18:00:56 -0700

    change cors doc to avoid CORS warning

--- ghibli-movies-frontend/src/index.js
+++ ghibli-movies-frontend/src/index.js
@@ -1,1 +1,4 @@
-console.log("testing...")
+const BACKEND_URL = "http://localhost:3000";
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
