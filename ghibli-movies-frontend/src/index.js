bc1040d76c61dcffe52d15eba6b7661d27f02b54
chanwkkk <chanwk18@hotmail.com>
2020-01-28 12:55:41 -0700

    add fetch movies on DOMContent loaded

--- ghibli-movies-frontend/src/index.js
+++ ghibli-movies-frontend/src/index.js
@@ -1,4 +1,27 @@
 const BACKEND_URL = "http://localhost:3000";
-fetch(`${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+const cardArea = document.getElementById("card-area")
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  fetchMovies()
+});
+
+  function fetchMovies(){
+    fetch(`${BACKEND_URL}/movies`)
+    .then(response => response.json())
+    .then(json => {
+      for (movie of json){
+        addMovietoPage(movie)
+      } 
+    }   
+      );
+  }
+
+  // function addMovietoPage(movie){
+  //   let div = document.createElement('div')
+  //   let p = document.createElement('p')
+  //   p.innerText = movie.description
+  //   div.append(p)
+  //   cardArea.append(div)
+  // }
+
