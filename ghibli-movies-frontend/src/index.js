1b2486241d8d12f44e4849a03237306f6ef05442
chanwkkk <chanwk18@hotmail.com>
2020-01-28 13:13:58 -0700

    add rt score,release year and director to display movies

--- ghibli-movies-frontend/src/index.js
+++ ghibli-movies-frontend/src/index.js
@@ -17,11 +17,28 @@ document.addEventListener("DOMContentLoaded", ()=>{
       );
   }
 
-  // function addMovietoPage(movie){
-  //   let div = document.createElement('div')
-  //   let p = document.createElement('p')
-  //   p.innerText = movie.description
-  //   div.append(p)
-  //   cardArea.append(div)
-  // }
+  function addMovietoPage(movie){
+    let div = document.createElement('div')
+    div.setAttribute("id", movie.title)
+    div.setAttribute("class","movie-card")
+
+    let p = document.createElement('p')
+    p.innerHTML = `
+    <h2>Title: ${movie.title}</h2>
+    <p>Rotten Tomato Score: ${movie.rt_score}<p>
+    <p>Release year: ${movie.release_year}<p>
+    <p>Director: ${movie.director.name}<p>
+    
+    <em>Description: ${movie.description}</br>
+    <img src="https://m.media-amazon.com/images/M/MV5BNTg0NmI1ZGQtZTUxNC00NTgxLThjMDUtZmRlYmEzM2MwOWYwXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY268_CR2,0,182,268_AL_.jpg" />
+    
+    
+    
+    
+    `
+    
+
+    div.append(p)
+    cardArea.append(div)
+  }
 
