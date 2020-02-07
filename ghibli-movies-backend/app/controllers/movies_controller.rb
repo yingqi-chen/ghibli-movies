86eea6ebb21b0bdd0231d154119d91e6f21da6ee
chanwkkk <chanwk18@hotmail.com>
2020-02-07 00:10:19 -0700

    add method in create method so as to prevent from creating empty character

--- ghibli-movies-backend/app/controllers/movies_controller.rb
+++ ghibli-movies-backend/app/controllers/movies_controller.rb
@@ -11,19 +11,14 @@ class MoviesController < ApplicationController
     end
 
     def show
-        movie = Movie.find(28)
-        render json: movie.to_json(
-            :include => {
-                :characters =>{ :except => [:created_at, :updated_at] },
-                :director =>{ :except => [:created_at, :updated_at]}
-        },   :except =>[:director_id, :created_at, :updated_at]
-        )
 
     end
 
     def create
-        binding.pry
         movie = Movie.create(movie_params)
+        if params["movie"]["characters_attributes"]['0']["name"] == ""
+          Character.last.destroy
+        end
     end
 
 private
--- ghibli-movies-backend/app/models/character.rb
+++ ghibli-movies-backend/app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ApplicationRecord
     belongs_to :movie
+    
 end
